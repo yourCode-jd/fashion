c289095b67c87656329eae63f49ff4ec957f15b5
jaspreet tpss <jaspreetstpss@gmail.com>
2024-10-04 15:34:53 +0530

    my new testing  project all files

---
+++ js/custom.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+  $(".products_list").slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    centerMode: true,
+    focusOnSelect: true,
+    rows: 0,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  $(".testimonial_list").slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false,
+    rows: 0,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+});
